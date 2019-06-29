8d3b9c0fc172c7622f4b2d2339aa361674a228d9
TomozoP <gigantelephant@gmail.com>
2019-06-29 13:55:48 +0900

    サンプルシーンを動くようにしました

---
+++ AngelHack/Assets/Scripts/VRSetting.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRSetting : MonoBehaviour
+{
+  void Start() {
+    OVRManager.tiledMultiResLevel = OVRManager.TiledMultiResLevel.LMSHigh;
+    UnityEngine.XR.XRSettings.eyeTextureResolutionScale = 1.75f;
+    OVRPlugin.chromatic = true;
+    QualitySettings.antiAliasing = 4;
+    OVRManager.display.displayFrequency = 72f;
+    OVRManager.cpuLevel = 2;
+    OVRManager.gpuLevel = 2;
+  }
+}
