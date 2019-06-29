9ad072b7d0fad3f4a7f82078406cff3654ff1a89
inolalala <inoue-sota@kayac.com>
2019-06-29 16:51:53 +0900

    一回公式の消してディクラさんの導入した

---
+++ AngelHack/Assets/Project/FirebaseConnectionSample/SimpleFirebaseSample.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleFirebaseUnity;
+
+public class SimpleFirebaseSample : MonoBehaviour
+{
+    Firebase firebase;
+    void Start()
+    {
+        firebase = Firebase.CreateNew("https://angelhack2019-d8bc0.firebaseio.com");  // 自分のFirebaseのアドレスを入れる
+        
+        firebase.OnGetSuccess += (Firebase f,DataSnapshot d) => 
+        {
+            Debug.Log(d.Value<string>());
+        };
+        firebase.Child("app").Child("goods").GetValue();
+    }
+}
