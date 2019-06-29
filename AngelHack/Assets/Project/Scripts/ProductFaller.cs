2dd47eb11f03a90e1c9db38e6948cd57ee77a14f
井上敦登 <inoue-atsuto@kayac.com>
2019-06-29 16:30:59 +0900

    落ちるサンプルプログラムの型を作った

---
+++ AngelHack/Assets/Project/Scripts/ProductFaller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductFaller : MonoBehaviour
+{
+  [SerializeField]
+  GameObject upperArea;
+
+  [SerializeField]
+  GameObject lowerArea;
+
+  [SerializeField]
+  List<GameObject> productPrefabs = new List<GameObject>();
+
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+  }
+
+  void CreateProductInstance(int modalId)
+  {
+
+  }
+}
