ddf0856855ee227c4185e8ce8b365fa2c713104e
inolalala <inoue-sota@kayac.com>
2019-06-30 02:51:34 +0900

    要素数までは取れるのでその中身を抜き出したい（あつお頼んだ・・・・）

---
+++ AngelHack/Assets/Project/FirebaseConnection/FirebaseDataGateway.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using SimpleFirebaseUnity;
+
+
+public class FirebaseDataGateway : SimpleFirebaseMonobehavior
+{
+    public List<GoodsItem> parsedItems = new List<GoodsItem>();
+    private Firebase firebase;
+    private Firebase app;
+    private Firebase goods;
+
+    void Start()
+    {
+        Initialize("https://angelhack2019-d8bc0.firebaseio.com");
+        StartCoroutine(Request(2f));
+    }
+
+    private void Initialize(string projectPath)
+    {
+        firebase = Firebase.CreateNew(projectPath);
+
+        // Init callbacks
+        firebase.OnGetSuccess += GetOKHandler;
+        firebase.OnGetFailed += GetFailHandler;
+        firebase.OnSetSuccess += SetOKHandler;
+        firebase.OnSetFailed += SetFailHandler;
+        firebase.OnUpdateSuccess += UpdateOKHandler;
+        firebase.OnUpdateFailed += UpdateFailHandler;
+        firebase.OnPushSuccess += PushOKHandler;
+        firebase.OnPushFailed += PushFailHandler;
+        firebase.OnDeleteSuccess += DelOKHandler;
+        firebase.OnDeleteFailed += DelFailHandler;
+
+        app = firebase.Child("app",true);
+        goods = app.Child("goods",true);
+
+        onGetOK += (dic,keys) => 
+        {
+            Debug.Log("ssssssssssss"+dic["dfdertaerff"]);
+            
+        };
+    }
+
+    public void RequestValue(Firebase target)
+    { 
+        target.GetValue();
+    }
+    public void RequestValueFromChild(Firebase targetRoot, string targetKey)
+    {
+        targetRoot.Child(targetKey,true).GetValue();
+    }
+
+    private IEnumerator Request(float waitTime)
+    {
+        while(true)
+        {
+            RequestValue(goods);
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+}
---
+++ AngelHack/Assets/Project/FirebaseConnection/SimpleFirebaseMonobehavior.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleFirebaseUnity;
+
+public abstract class SimpleFirebaseMonobehavior : MonoBehaviour
+{
+
+    public delegate void OnGetOK(Dictionary<string,object> d, List<string> key);
+    public event OnGetOK onGetOK;
+
+    protected void GetOKHandler(Firebase sender, DataSnapshot snapshot)
+    {
+        DebugLog("[OK] Get from key: <" + sender.FullKey + ">");
+        DebugLog("[OK] Raw Json: " + snapshot.RawJson);
+
+        Dictionary<string, object> dict = snapshot.Value<Dictionary<string, object>>();
+        List<string> keys = snapshot.Keys;
+        Debug.Log(dict.Count);
+        if (keys != null)
+            foreach (string key in keys)
+            {
+                DebugLog(key + " = " + dict[key].ToString());
+                //Debug.Log("wwwwwwwwwww "+dict.Values.Count);
+
+
+                foreach(var d2 in dict.Values)
+                {
+                    //Debug.Log("fffffffffff ");
+                    
+                }
+            }
+        if(onGetOK != null)
+        {
+            onGetOK(dict,keys);
+        }
+    }
+
+    protected void GetFailHandler(Firebase sender, FirebaseError err)
+    {
+        DebugError("[ERR] Get from key: <" + sender.FullKey + ">,  " + err.Message + " (" + (int)err.Status + ")");
+    }
+
+    protected void SetOKHandler(Firebase sender, DataSnapshot snapshot)
+    {
+        DebugLog("[OK] Set from key: <" + sender.FullKey + ">");
+    }
+
+    protected void SetFailHandler(Firebase sender, FirebaseError err)
+    {
+        DebugError("[ERR] Set from key: <" + sender.FullKey + ">, " + err.Message + " (" + (int)err.Status + ")");
+    }
+
+    protected void UpdateOKHandler(Firebase sender, DataSnapshot snapshot)
+    {
+        DebugLog("[OK] Update from key: <" + sender.FullKey + ">");
+    }
+
+    protected void UpdateFailHandler(Firebase sender, FirebaseError err)
+    {
+        DebugError("[ERR] Update from key: <" + sender.FullKey + ">, " + err.Message + " (" + (int)err.Status + ")");
+    }
+
+    protected void DelOKHandler(Firebase sender, DataSnapshot snapshot)
+    {
+        DebugLog("[OK] Del from key: <" + sender.FullKey + ">");
+    }
+
+    protected void DelFailHandler(Firebase sender, FirebaseError err)
+    {
+        DebugError("[ERR] Del from key: <" + sender.FullKey + ">, " + err.Message + " (" + (int)err.Status + ")");
+    }
+
+    protected void PushOKHandler(Firebase sender, DataSnapshot snapshot)
+    {
+        DebugLog("[OK] Push from key: <" + sender.FullKey + ">");
+    }
+
+    protected void PushFailHandler(Firebase sender, FirebaseError err)
+    {
+        DebugError("[ERR] Push from key: <" + sender.FullKey + ">, " + err.Message + " (" + (int)err.Status + ")");
+    }
+
+    protected void GetRulesOKHandler(Firebase sender, DataSnapshot snapshot)
+    {
+        DebugLog("[OK] GetRules");
+        DebugLog("[OK] Raw Json: " + snapshot.RawJson);
+    }
+
+    protected void GetRulesFailHandler(Firebase sender, FirebaseError err)
+    {
+        DebugError("[ERR] GetRules,  " + err.Message + " (" + (int)err.Status + ")");
+    }
+
+
+    protected void DebugLog(string str)
+    {
+        Debug.Log(str);
+    }
+
+    protected void DebugWarning(string str)
+    {
+        Debug.LogWarning(str);
+    }
+
+    protected void DebugError(string str)
+    {
+        Debug.LogError(str);
+    }
+}
