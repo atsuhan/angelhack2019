d315db8e9e38b0bb64bc431cb1642ac59e6560c4
井上敦登 <inoue-atsuto@kayac.com>
2019-06-29 18:18:19 +0900

    upperPlaneの範囲でインスタンス化されるようになった

--- AngelHack/Assets/Project/Scripts/ProductFaller.cs
+++ AngelHack/Assets/Project/Scripts/ProductFaller.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class ProductFaller : MonoBehaviour
 {
   [SerializeField]
+  GameObject instanceParrent;
+
+  [SerializeField]
   GameObject upperArea;
 
   [SerializeField]
@@ -21,14 +24,29 @@ public class ProductFaller : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-    if (Input.GetKey(KeyCode.Space))
+    if (Input.GetKeyDown(KeyCode.Space))
     {
-      CreateProductInstance(1);
+      InstantiateProductPrefab(1);
     }
   }
 
-  void CreateProductInstance(int modalId)
+  void InstantiateProductPrefab(int modalId)
   {
-    Instantiate(productPrefabs[modalId], Vector3.zero, Quaternion.identity, upperArea.transform);
+    Instantiate(
+      productPrefabs[modalId],
+      GetProductInitRandomPos(),
+      Random.rotation,
+      instanceParrent.transform
+    );
+  }
+
+  Vector3 GetProductInitRandomPos()
+  {
+    Vector3 upperPos = upperArea.transform.position;
+    float upperLengthX = upperArea.transform.localScale.x * 10;
+    float upperLengthZ = upperArea.transform.localScale.z * 10;
+    float posX = Random.Range(upperPos.x - (upperLengthX / 2), upperPos.x + (upperLengthX / 2));
+    float posZ = Random.Range(upperPos.z - (upperLengthZ / 2), upperPos.z + (upperLengthZ / 2));
+    return new Vector3(posX, upperPos.y, posZ);
   }
 }
